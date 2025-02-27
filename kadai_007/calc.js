dbbcca1da2a1714662c3d1bb1ac3ae4553a36606
Ayumi <nabeco91@gmail.com>
2025-02-27 21:35:41 +1000

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+let num =30;
+
+console.log(num);
+
+if (num % 3 == 0) {
+  console.log ("3の倍数です")
+}
+
+if (num % 3 == 0) {
+  console.log ("5の倍数です")
+}
+
+if (num % 15 == 0) {
+  console.log ("3と５の倍数です")
+}
+
+else [
+  console.log (num)
+]
+
